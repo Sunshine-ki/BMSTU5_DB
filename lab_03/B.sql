f73bfec030e6e5d8defcdc60cf11134bc5d336a3
Sunshine-ki <sukocheva.alis@mail.ru>
2020-10-27 01:50:40 +0300

    Ещё чуть-чуть...

--- lab_03/B.sql
+++ lab_03/B.sql
@@ -101,10 +101,11 @@ CREATE OR REPLACE PROCEDURE proc_update_cursor
     new_invited_id INT
 )
 AS '
-DECLARE myCursor CURSOR FOR
-    SELECT *
-    FROM user_tmp
-    WHERE invited_id = in_invited_id;
+DECLARE
+    myCursor CURSOR FOR
+        SELECT *
+        FROM user_tmp
+        WHERE invited_id = in_invited_id;
     tmp user_tmp;
 BEGIN
     OPEN myCursor;
@@ -132,16 +133,34 @@ CALL proc_update_cursor(1, 0);
 
 SELECT * FROM user_tmp;
 
+-- B.4. Хранимую процедуру доступа к метаданным.
+-- Получаем название атрибутов и их тип по .
+CREATE OR REPLACE PROCEDURE metadata(name VARCHAR) -- Получает название таблицы
+AS '
+    -- Инфа про метаданные:
+    -- https://postgrespro.ru/docs/postgresql/9.6/infoschema-columns
+    DECLARE
+        myCursor CURSOR FOR
+            SELECT column_name,
+                   data_type
+           -- INFORMATION_SCHEMA обеспечивает доступ к метаданным о базе данных.
+           -- columns - данные о столбацых.
+            FROM information_schema.columns
+            WHERE table_name = name;
+        -- RECORD - переменная, которая подстравивается под любой тип.
+        tmp RECORD;
+BEGIN
+        OPEN myCursor;
 
+        LOOP
+            FETCH myCursor
+            INTO tmp;
+            EXIT WHEN NOT FOUND;
+            RAISE NOTICE ''column name = %; data type = %'', tmp.column_name, tmp.data_type;
+        END LOOP;
 
+        CLOSE myCursor;
+END;
+' LANGUAGE plpgsql;
 
-
-
-
-
-
-
-
-
-
-
+CALL metadata('users');
--- lab_03/C.sql
+++ lab_03/C.sql
@@ -1,3 +1,43 @@
+-- Триггер - Объект. Ответ на событие.
+-- Инфа про триггеры:
+-- https://postgrespro.ru/docs/postgresql/9.6/sql-createtrigger
+
+-- SELECT * INTO user_tmp
+-- FROM userstest;
+
 -- C.1. Триггер AFTER.
+-- Когда в таблице меняется id человека (на new.id),
+-- То нужно поменять у всех остальных юзеров invited_id на new.id.
+-- Т.е. поменять id того человека, который их пригласил
+CREATE OR REPLACE FUNCTION update_trigger()
+RETURNS TRIGGER
+AS '
+BEGIN
+    RAISE NOTICE ''New =  %'', new;
+    RAISE NOTICE ''Old =  %'', old;
+
+    UPDATE user_tmp
+    SET invited_id = new.id
+    WHERE user_tmp.invited_id = old.id;
+
+    --     Для операций INSERT и UPDATE возвращаемым значением должно быть NEW.
+    RETURN new;
+END;
+' LANGUAGE plpgsql;
+
+-- AFTER - оперделяет, что заданная цункция будет вызываться после события.
+CREATE TRIGGER log_update
+AFTER UPDATE ON user_tmp
+-- Триггер с пометкой FOR EACH ROW вызывается один раз для каждой строки,
+-- изменяемой в процессе операции.
+FOR EACH ROW
+EXECUTE PROCEDURE update_trigger();
+
+
+UPDATE user_tmp
+SET id = 1
+WHERE id = 15;
+
+SELECT * FROM user_tmp;
 
 -- C.2. Триггер INSTEAD OF.
