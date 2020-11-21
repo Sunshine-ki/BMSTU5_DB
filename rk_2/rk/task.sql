136407450de009f8b45ef299aa6423131870b031
Sunshine-ki <sukocheva.alis@mail.ru>
2020-11-21 18:57:23 +0300

    Update

--- rk_2/rk/task.sql
+++ rk_2/rk/task.sql
@@ -1,5 +1,6 @@
 -- Вариант 2
 
+-- Виды работ.
 CREATE TABLE IF NOT EXISTS type_of_jobs
 (
     id INT PRIMARY KEY,
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS type_of_jobs
     equipment VARCHAR(32) -- Необходимое оборудование.
 );
 
--- Развязочная таблица для type_of_jobs и executor
+-- Развязочная таблица для type_of_jobs и executor.
 CREATE TABLE IF NOT EXISTS type_of_jobs_executor
 (
     id_type_of_jobs INT,
@@ -55,3 +56,29 @@ CREATE TABLE IF NOT EXISTS type_of_jobs_customer
     id_customer INT,
     FOREIGN KEY (id_customer) REFERENCES customer(id)
 );
+
+-- ЗАПРОСЫ
+
+-- 1. Инструкция SELECT, использующая предикат сравнения.
+-- Вывести всю информацию о исполнителях
+-- Год рождения которых меньше 1995.
+SELECT *
+FROM executor
+WHERE year_of_birth < 1995; -- AND year_of_birth < 2000;
+
+-- 2. Инструкцию, использующую оконную функцию.
+-- Вывести информацию о виде работы,
+-- Добавляя столбец cnt, в котором показано
+-- Сколько сумерно тратится (сумма трудозатрат)
+-- На работу с каким-то оборудованием.
+SELECT id, name_jobs, labor_costs, equipment, SUM(labor_costs) OVER(PARTITION BY equipment) cnt
+FROM type_of_jobs
+ORDER BY cnt;
+
+-- 3. Инструкцию SELECT, консолидирующую данные с помощью
+-- предложения GROUP BY и предложения HAVING
+-- Получить для каждого стажа
+SELECT experience, COUNT(experience) cnt
+FROM customer
+GROUP BY experience
+HAVING COUNT(experience) > 2;
