49be1c6b32585e1b760d37ee01433aff17fd74fb
Sunshine-ki <sukocheva.alis@mail.ru>
2020-12-19 14:04:29 +0300

    Update

--- rk_3/rk/lis/requests_sql.py
+++ rk_3/rk/lis/requests_sql.py
@@ -20,10 +20,10 @@ LIMIT 1;
 TASK_2_3 = """
 SELECT e.id, e.fio 
 FROM employee AS e 
-JOIN employee_attendance AS e_a ON (e_a.employee_id = e.id) 
-WHERE ((e_a.time > '09:00:00') AND (e_a.type = 1)) 
+JOIN employee_attendance AS e_a ON e_a.employee_id = e.id
+WHERE (e_a.time > '09:00:00') AND (e_a.type = 1) 
 GROUP BY e.id, e.fio 
-HAVING (Count(e.id) > 2);
+HAVING (Count(e.id) > 5);
 """
 
 
